583dac49a4df4a604a596c68c1be6bc578893ade
Yonatan Kra <me@yonatankra.com>
2024-08-31 16:34:52 +0300

    Change load page

--- apps/custom-talisman-cards/src/server/api/greet.ts
+++ apps/custom-talisman-cards/src/server/api/greet.ts
@@ -2,9 +2,8 @@ import { defineEventHandler, getQuery } from 'h3';
 
 export default defineEventHandler((event) => {
   const q = getQuery(event);
-  const projectName = q.name || 'World';
 
   return {
-    message: `Hello ${projectName}`,
+    message: `Hello Visitor!`,
   };
 });
